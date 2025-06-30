3be591636d9cb82aac16260ef0898a46fc9f2f99
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-06-30 20:13:13 +0200

    feat: add app routing for login, signup, dashboard and page view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageComponent } from './components/page/page.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'page/:id', component: PageComponent },
+  { path: '**', redirectTo: 'login' }, // fallback 404
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
