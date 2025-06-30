219a5f1e6707d1fc524bd704279fd857a51b1fe0
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-06-30 20:06:14 +0200

    feat: add base components - login, signup, dashboard, page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PageComponent } from './components/page/page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    PageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
